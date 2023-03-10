ded86bdf1268c9059bbeb8e9a5897133dd880194
Marcela Pernicová <marcela.pernicova4@gmail.com>
2023-03-10 17:47:48 +0100

    změny

--- src/main/java/cz/czechitas/ukol3/HlavniProgram.java
+++ src/main/java/cz/czechitas/ukol3/HlavniProgram.java
@@ -3,12 +3,41 @@ package cz.czechitas.ukol3;
 /**
  * Spouštěcí třída celého programu
  */
+
+
+
+
+
 public class HlavniProgram {
 
     public static void main(String... args) {
 
         //TODO tady bude váš kód - vytvoření instance třídy Pocitac, zapnutí, vpynutí, výpis hodnot.
         System.out.println("Program spuštěn.");
+
+        Pocitac marcinPocitac = new Pocitac();
+//        System.out.println(marcinPocitac.toString());
+////       marcinPocitac.zapniSe(null,null,null);
+
+        Disk marcinDisk = new Disk();
+        marcinDisk.setKapacita(254_548_554_547L);
+        marcinDisk.setVyuziteMisto(125_256_458_230L);
+
+        Pamet marcinaPamet = new Pamet();
+        marcinaPamet.setKapacita(8_000_000_000L);
+
+
+        Procesor marcinProcesor = new Procesor();
+        marcinProcesor.setRychlost(5_300_000_000_000L);
+        marcinProcesor.setVyrobce("AMD");
+
+        marcinPocitac.setPevnyDisk(marcinDisk);
+        marcinPocitac.setRam(marcinaPamet);
+        marcinPocitac.setCpu(marcinProcesor);
+
+
+        System.out.println(marcinPocitac.toString());
+        marcinPocitac.zapniSe();
     }
 
 }
--- src/main/java/cz/czechitas/ukol3/Pocitac.java
+++ src/main/java/cz/czechitas/ukol3/Pocitac.java
@@ -1,25 +1,68 @@
 package cz.czechitas.ukol3;
 
+import static java.util.Objects.isNull;
+
 public class Pocitac {
+
+    private Procesor mujProcesor;
+    private Pamet mojePamet;
+    private Pamet ram;
+    private Disk mujDisk;
+
+    private Disk pevnyDisk;
+
+    private Procesor cpu;
+
+
 
     public boolean isJeZapnuty() {
         return this.jeZapnuty;
     }
+    public void setCpu(Procesor procesor){
+        this.cpu=procesor;
+    }
 
-    public void zapniSe(){
-        if (jeZapnuty){
-            System.err.println("Počítač je už zapnutý.");
+    public void setPevnyDisk(Disk disk){
+        this.pevnyDisk=disk;
+    }
+    public void setRam(Pamet pamet){
+        this.ram=pamet;
+    }
+    public void zapniSe() {
+        if (jeZapnuty) {
+            if (this.mujProcesor==null||this.mojePamet==null ||this.mujDisk==null) {
+                System.err.println("Komponenty počítače se nenačetly.");
+                return;
+            }
+
+            System.out.println("Počítač se zapnul.");
         } else {
-            zapniSe();
+            System.err.println("Počítač je už zapnutý.");
         }
 
     }
-    public void vypniSe(){
-    if (jeZapnuty){
-        vypniSe();
-    } else {
+
+    public void vypniSe() {
+        if (jeZapnuty) {
+            vypniSe();
+        } else {
             System.err.println("Počítač je už vypnutý.");
         }
     }
-    private boolean jeZapnuty;
+
+    public boolean jeZapnuty;
+
+    {
+        if (jeZapnuty) {
+            System.out.println("Počítač je zapnutý.");
+
+        } else {
+            System.out.println("Počítač je vypnutý.");
+
+        }
+    }
 }
+
+
+
+
